c516110aeeb33a53afe2d105c959a4bf7842dbfb
etherblood <philipp@jobach.de>
2020-11-14 19:22:17 +0100

    woodCount util method

--- parent/bot/src/main/java/com/etherblood/etherchess/bot/evaluation/WoodCountEvaluation.java
+++ parent/bot/src/main/java/com/etherblood/etherchess/bot/evaluation/WoodCountEvaluation.java
@@ -1,15 +1,17 @@
 package com.etherblood.etherchess.bot.evaluation;
 
 import com.etherblood.etherchess.engine.State;
+import com.etherblood.etherchess.engine.util.Piece;
 import com.etherblood.etherchess.engine.util.SquareSet;
 
 public class WoodCountEvaluation implements Evaluation {
 
-    private static final int QUEEN_SCORE = 900;
-    private static final int ROOK_SCORE = 500;
-    private static final int BISHOP_SCORE = 310;
-    private static final int KNIGHT_SCORE = 290;
-    private static final int PAWN_SCORE = 100;
+    public static final int KING_SCORE = 15_000;
+    public static final int QUEEN_SCORE = 900;
+    public static final int ROOK_SCORE = 500;
+    public static final int BISHOP_SCORE = 310;
+    public static final int KNIGHT_SCORE = 290;
+    public static final int PAWN_SCORE = 100;
 
     @Override
     public int evaluate(State state) {
@@ -27,5 +29,26 @@ public class WoodCountEvaluation implements Evaluation {
         sum += KNIGHT_SCORE * SquareSet.count(state.knights() & owner);
         sum += PAWN_SCORE * SquareSet.count(state.pawns() & owner);
         return sum;
+    }
+
+    public static int score(int piece) {
+        switch (piece) {
+            case Piece.EMPTY:
+                return 0;
+            case Piece.PAWN:
+                return PAWN_SCORE;
+            case Piece.KING:
+                return KING_SCORE;
+            case Piece.KNIGHT:
+                return KNIGHT_SCORE;
+            case Piece.BISHOP:
+                return BISHOP_SCORE;
+            case Piece.ROOK:
+                return ROOK_SCORE;
+            case Piece.QUEEN:
+                return QUEEN_SCORE;
+            default:
+                throw new AssertionError(piece);
+        }
     }
 }
